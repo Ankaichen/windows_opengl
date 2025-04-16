1d7c6b718ea049867298841d9d85e633c592e2d2
ankaichen <2751551364akc@gmail.com>
2025-04-16 21:13:06 +0800

    add assimp

---
+++ utils/assimp_loader.h
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file           : assimp_loader.h
+  * @author         : An Kaichen
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2025-04-16
+  ******************************************************************************
+  */
+
+#ifndef OPENGL_ASSIMP_LOADER_H
+#define OPENGL_ASSIMP_LOADER_H
+
+
+class AssimpLoader {
+
+};
+
+
+#endif //OPENGL_ASSIMP_LOADER_H
